562f55326492473eb43e4207af06723957b046e5
Brian Sorahan <bsorahan@gmail.com>
2015-02-15 20:40:07 -0600

    initialize timer id

--- posix/metro.c
+++ posix/metro.c
@@ -6,6 +6,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TIMER_CREATE_SUCCESS 0
+
+static void
+timer_tick(union sigval arg);
+
 struct Metro {
     timer_t *tid;
 };
@@ -15,10 +20,28 @@ Metro_create()
 {
     Metro metro;
     NEW(metro);
+
+    struct sigevent tick;
+    tick.sigev_notify = SIGEV_THREAD;
+    tick.sigev_value.sival_ptr = NULL;
+    tick.sigev_notify_function = timer_tick;
+    
+    timer_t tid;
+    int rc = timer_create(CLOCK_REALTIME, &tick, &tid);
+
+    if (rc != TIMER_CREATE_SUCCESS) {
+        return NULL;
+    }
+    
     return metro;
 }
 
 void
 Metro_free(Metro *metro)
+{
+}
+
+static void
+timer_tick(union sigval arg)
 {
 }
